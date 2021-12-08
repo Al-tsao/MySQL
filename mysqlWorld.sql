aee69770d1a53fc398191595686b3bf4ac44a95c
AlTsao <al.tsao.yw@gmail.com>
2021-12-08 11:09:08 +0800

    revise

---
+++ mysqlWorld.sql
@@ -0,0 +1,9 @@
+SELECT 
+        Name,
+        GNP,
+        Continent,
+        Population,
+        GNP / Population AS GNP_percap #可以做運算，然後用AS重新命名經過運算後新欄位的名稱
+    FROM 
+        world.country; #記得要用;作為這個區塊的結束
+   
