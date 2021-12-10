75bd390c152200520759ba6a07fc060de2d4f990
AlTsao <al.tsao.yw@gmail.com>
2021-12-10 11:45:51 +0800

    revise

--- 12_mysql_selectFrom.sql
+++ 12_mysql_selectFrom.sql
@@ -6,4 +6,5 @@ SELECT
         GNP / Population AS GNP_percap, #可以做運算，然後用AS重新命名經過運算後新欄             
     FROM 
         world.country; #記得要用;作為這個區塊的結束
-   
+
+#SELECT是選擇欄位資料
---
+++ 16_mysql_where1.sql
@@ -0,0 +1,9 @@
+SELECT 
+        Name,
+        Population
+    FROM 
+        world.country
+    WHERE
+        Name = 'France';
+   
+#WHERE: 做水平的資料篩選
