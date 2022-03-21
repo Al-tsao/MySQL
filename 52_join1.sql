1c9e440072cf6e716c86b7c308cc96945fd8ba93
Al-tsao <al.tsao.yw@gmail.com>
2022-03-21 09:50:30 +0100

    revise

---
+++ 52_join1.sql
@@ -0,0 +1,11 @@
+/*
+SELECT 
+        left_tbl.VARIABLE,
+        right_tbl.VARIBLE
+    FROM
+        left_tbl
+    JOIN
+        right_tbl #預設是inner join
+    ON
+        left_tbl.FK = right_tbl.FK;
+*/
