2e9de235df9fca05a90f34557a7cfe05674eded3
Al-tsao <al.tsao.yw@gmail.com>
2022-04-05 16:00:04 +0200

    revise

---
+++ 58_join4.sql
@@ -0,0 +1,22 @@
+/*左外部聯結*/
+SELECT 
+        left_tbl.*,
+        right_tbl.*
+    FROM 
+        (SELECT 
+                *
+            FROM
+                world.country
+            WHERE 
+                Code IN ('TWN', 'JPN')
+    ) AS left_tbl
+    RIGHT JOIN 
+        (SELECT
+                *
+            FROM
+                world.city
+            WHERE
+            CountryCode IN ('TWN', 'KOR')
+    ) AS right_tbl
+    ON
+        left_tbl.Code = right_tbl.CountryCode;
